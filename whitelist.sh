85a92c650e5f2cb8a774e53fedd175b9d97dff6a
kabarton62 <kbarton@tamusa.edu>
2020-09-29 08:04:37 -0600

    Create whitelist.sh

---
+++ whitelist.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+#whitelist the vpn and LAN
+echo 172.17.128.3 >> /etc/sshguard/whitelist
+echo 172.20.0.0/16 >> /etc/sshguard/whitelist
+service sshguard restart
+service ssh restart
