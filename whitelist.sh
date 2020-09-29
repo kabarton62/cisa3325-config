70b09131565566359bfa2a9c670087169c336e61
kabarton62 <kbarton@tamusa.edu>
2020-09-29 08:08:56 -0600

    Update whitelist.sh

--- whitelist.sh
+++ whitelist.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 #whitelist the vpn and LAN
+sudo su
 echo 172.17.128.3 >> /etc/sshguard/whitelist
 echo 172.20.0.0/16 >> /etc/sshguard/whitelist
 service sshguard restart
 service ssh restart
+exit
